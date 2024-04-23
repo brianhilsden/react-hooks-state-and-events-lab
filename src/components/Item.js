a4660f851e7660720516037edcfa949d49267e67
Brian Omondi <brianhilsden@gmail.com>
2024-04-23 15:14:42 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -9,7 +9,7 @@ function Item({ name, category}) {
     <li className={addToCart?"in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add" onClick={handleClick}>{addToCart? "Remove From" : "Add to"} Cart</button>
+      <button className="add" onClick={handleClick}>{addToCart? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
